8378985571ee9fee0f320d64297103b644e85e80
timmaxx <m.n.timchenko@gmail.com>
2025-09-23 23:10:25 +0300

    implement "dirty read problem" as junit test

--- src/test/java/com/timmax/training_demo/transaction_isolation_level/DataBaseTest.java
+++ src/test/java/com/timmax/training_demo/transaction_isolation_level/DataBaseTest.java
@@ -171,4 +171,46 @@ public class DataBaseTest {
 
         Assertions.assertEquals(ONE_RECORD_AFTER_FIRST_UPDATE_IMMUTABLE_DB_TABLE, workDbTable);
     }
+
+    @Test
+    public void dirtyReadProblem() {
+        final DbTable workDbTable = new DbTable(ONE_RECORD_AFTER_FIRST_INSERT_IMMUTABLE_DB_TABLE);
+
+        final SQLCommandQueue sqlCommandQueue1 = new SQLCommandQueue();
+        sqlCommandQueue1.add(
+                new SQLCommandUpdate(
+                        workDbTable,
+                        1,
+                        oldDbRecord -> new DbRecord(oldDbRecord.field1() + 111)
+                )
+        );
+        sqlCommandQueue1.add(
+                new SQLCommandSleep(
+                        null,
+                        300
+                ));
+        sqlCommandQueue1.startThread();
+
+        final SQLCommandQueue sqlCommandQueue2 = new SQLCommandQueue();
+        sqlCommandQueue2.add(
+                new SQLCommandSleep(
+                        null,
+                        200
+                ));
+        sqlCommandQueue2.add(
+                new SQLCommandSelect(
+                        workDbTable,
+                        1
+                ));
+        sqlCommandQueue2.startThread();
+
+        sqlCommandQueue1.joinToThread();
+        sqlCommandQueue2.joinToThread();
+
+        sqlCommandQueue1.rollback();
+
+        DbTable middleDbTableResultInTransaction2 = new DbTable();
+        middleDbTableResultInTransaction2.insert(sqlCommandQueue2.popFromDbRecordResultLog());
+        Assertions.assertEquals(ONE_RECORD_AFTER_FIRST_UPDATE_IMMUTABLE_DB_TABLE, middleDbTableResultInTransaction2);
+    }
 }
