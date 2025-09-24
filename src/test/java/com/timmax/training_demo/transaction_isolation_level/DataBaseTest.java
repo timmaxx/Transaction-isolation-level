46bfbf7a9f93270fdb35b8874d13aaa7a6d8d96d
timmaxx <m.n.timchenko@gmail.com>
2025-09-24 15:14:56 +0300

    prepare tests for "phantom reads problem"

--- src/test/java/com/timmax/training_demo/transaction_isolation_level/DataBaseTest.java
+++ src/test/java/com/timmax/training_demo/transaction_isolation_level/DataBaseTest.java
@@ -4,6 +4,7 @@ import com.timmax.training_demo.transaction_isolation_level.sqlcommand.*;
 import com.timmax.training_demo.transaction_isolation_level.table.DbRecord;
 import com.timmax.training_demo.transaction_isolation_level.table.DbTable;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -199,6 +200,79 @@ public class DataBaseTest {
 
         logger.debug("dbTableResultInTransaction1 = {}", dbTableResultInTransaction1);
         logger.debug("dbTableResultInTransaction2 = {}", dbTableResultInTransaction2);
+        //  ToDo:   Сделать проверку на то, что количество строк совпадает, но строки отличаются.
+        Assertions.assertNotEquals(dbTableResultInTransaction1, dbTableResultInTransaction2);
+    }
+
+    @Test
+    @Disabled
+    public void PhantomReadsProblemForZeroToOneRow() {
+        final DbTable workDbTable = new DbTable(EMPTY_IMMUTABLE_DB_TABLE);
+
+        final SQLCommandQueue sqlCommandQueue1 = new SQLCommandQueue();
+        sqlCommandQueue1.add(
+                new SQLCommandSelect(workDbTable, 1),
+                new SQLCommandSleep(null, 200),
+                new SQLCommandSelect(workDbTable, 1)
+        );
+        sqlCommandQueue1.startThread();
+
+        final SQLCommandQueue sqlCommandQueue2 = new SQLCommandQueue();
+        sqlCommandQueue2.add(
+                new SQLCommandSleep(null, 50),
+                new SQLCommandInsert(workDbTable, recordForOneInsert)
+        );
+        sqlCommandQueue2.startThread();
+
+        sqlCommandQueue1.joinToThread();
+        sqlCommandQueue2.joinToThread();
+
+
+        DbTable dbTableResultInTransaction2 = new DbTable();
+        dbTableResultInTransaction2.insert(sqlCommandQueue1.popFromDbRecordResultLog());
+
+        DbTable dbTableResultInTransaction1 = new DbTable();
+        dbTableResultInTransaction1.insert(sqlCommandQueue1.popFromDbRecordResultLog());
+
+        logger.info("dbTableResultInTransaction1 = {}", dbTableResultInTransaction1);
+        logger.info("dbTableResultInTransaction2 = {}", dbTableResultInTransaction2);
+        //  ToDo:   Сделать проверку на то, что количество строк НЕ совпадает.
+        Assertions.assertNotEquals(dbTableResultInTransaction1, dbTableResultInTransaction2);
+    }
+
+    @Test
+    @Disabled
+    public void PhantomReadsProblemForOneToTwoRows() {
+        final DbTable workDbTable = new DbTable(ONE_RECORD_AFTER_FIRST_INSERT_IMMUTABLE_DB_TABLE);
+
+        final SQLCommandQueue sqlCommandQueue1 = new SQLCommandQueue();
+        sqlCommandQueue1.add(
+                new SQLCommandSelect(workDbTable, 1),
+                new SQLCommandSleep(null, 200),
+                new SQLCommandSelect(workDbTable, 1)
+        );
+        sqlCommandQueue1.startThread();
+
+        final SQLCommandQueue sqlCommandQueue2 = new SQLCommandQueue();
+        sqlCommandQueue2.add(
+                new SQLCommandSleep(null, 50),
+                new SQLCommandInsert(workDbTable, recordForOneInsert)
+        );
+        sqlCommandQueue2.startThread();
+
+        sqlCommandQueue1.joinToThread();
+        sqlCommandQueue2.joinToThread();
+
+
+        DbTable dbTableResultInTransaction2 = new DbTable();
+        dbTableResultInTransaction2.insert(sqlCommandQueue1.popFromDbRecordResultLog());
+
+        DbTable dbTableResultInTransaction1 = new DbTable();
+        dbTableResultInTransaction1.insert(sqlCommandQueue1.popFromDbRecordResultLog());
+
+        logger.info("dbTableResultInTransaction1 = {}", dbTableResultInTransaction1);
+        logger.info("dbTableResultInTransaction2 = {}", dbTableResultInTransaction2);
+        //  ToDo:   Сделать проверку на то, что количество строк НЕ совпадает.
         Assertions.assertNotEquals(dbTableResultInTransaction1, dbTableResultInTransaction2);
     }
 }
