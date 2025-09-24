2f8d636c2e4408e1a7e53be044d5b201559f975e
timmaxx <m.n.timchenko@gmail.com>
2025-09-24 15:03:40 +0300

    edit ToDo in SQLCommand

--- src/main/java/com/timmax/training_demo/transaction_isolation_level/sqlcommand/SQLCommand.java
+++ src/main/java/com/timmax/training_demo/transaction_isolation_level/sqlcommand/SQLCommand.java
@@ -2,6 +2,16 @@ package com.timmax.training_demo.transaction_isolation_level.sqlcommand;
 
 import com.timmax.training_demo.transaction_isolation_level.table.BaseDbTable;
 
+//  ToDo:   сделать рефакторинг, на основе нижеописанного.
+//  Есть мнение, что SELECT относится к DQL (Data Query Language), но не к DML (Data Manipulation Language).
+//  Я вижу, что:
+//      -   DML команды (INSERT, UPDATE, DELETE)
+//          -   могут изменять данные, а значит порождают журнал изменения,
+//          -   не возвращают данные, а значит не содержат результата.
+//      -   DQL команда (SELECT)
+//          -   не изменяет данные, а значит НЕ порождает журнал изменения,
+//          -   возвращают данные, а значит содержит результат.
+//  Кроме того, сейчас имеется класс SQLCommandSleep, который вообще к SQL не относится.
 public abstract class SQLCommand implements RunnableWithLogAndDataResultOfSQLCommand {
     protected final BaseDbTable baseDbTable;
     protected RunnableWithLogAndDataResultOfSQLCommand runnable;
