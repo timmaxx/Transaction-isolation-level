780fec4ab2a98dbcb41f7fdf5c29ca44a7dbd5f2
timmaxx <m.n.timchenko@gmail.com>
2025-09-25 20:07:23 +0300

    implement "phantom reads problem" as junit test

--- src/test/java/com/timmax/training_demo/transaction_isolation_level/DataBaseTest.java
+++ src/test/java/com/timmax/training_demo/transaction_isolation_level/DataBaseTest.java
@@ -5,7 +5,6 @@ import com.timmax.training_demo.transaction_isolation_level.table.DbRecord;
 import com.timmax.training_demo.transaction_isolation_level.table.DbTable;
 import com.timmax.training_demo.transaction_isolation_level.table.ImmutableDbTable;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -308,67 +307,66 @@ public class DataBaseTest {
     }
 
     @Test
-    @Disabled
     public void PhantomReadsProblemForZeroToOneRow() {
         final DbTable workDbTable = new DbTable(EMPTY_IMMUTABLE_DB_TABLE);
         logger.info("workDbTable = {}", workDbTable);
 
         final SQLCommandQueue sqlCommandQueue1 = new SQLCommandQueue();
         sqlCommandQueue1.add(
-                new SQLCommandSelect(workDbTable, Set.of(1, 2)),
+                new SQLCommandSelect(workDbTable, Set.of(0, 1, 2)),
                 new SQLCommandSleep(null, 200),
-                new SQLCommandSelect(workDbTable, Set.of(1, 2))
+                new SQLCommandSelect(workDbTable, Set.of(0, 1, 2))
         );
-        sqlCommandQueue1.startThread();
 
         final SQLCommandQueue sqlCommandQueue2 = new SQLCommandQueue();
         sqlCommandQueue2.add(
                 new SQLCommandSleep(null, 50),
-                new SQLCommandInsert(workDbTable, recordForOneInsert)
+                new SQLCommandInsert(workDbTable, recordAfterOneUpdate)
         );
-        sqlCommandQueue2.startThread();
 
+        sqlCommandQueue1.startThread();
+        sqlCommandQueue2.startThread();
         sqlCommandQueue1.joinToThread();
         sqlCommandQueue2.joinToThread();
 
-        ImmutableDbTable dbTableResultInTransaction2 = sqlCommandQueue1.popFromImmutableDbTableResultLog();
+        ImmutableDbTable dbTableResultInTransaction3 = sqlCommandQueue1.popFromImmutableDbTableResultLog();
+        sqlCommandQueue1.popFromImmutableDbTableResultLog();
         ImmutableDbTable dbTableResultInTransaction1 = sqlCommandQueue1.popFromImmutableDbTableResultLog();
 
         logger.info("dbTableResultInTransaction1 = {}", dbTableResultInTransaction1);
-        logger.info("dbTableResultInTransaction2 = {}", dbTableResultInTransaction2);
-        Assertions.assertNotEquals(dbTableResultInTransaction1.count(), dbTableResultInTransaction2.count());
+        logger.info("dbTableResultInTransaction3 = {}", dbTableResultInTransaction3);
+        Assertions.assertNotEquals(dbTableResultInTransaction1.count(), dbTableResultInTransaction3.count());
     }
 
     @Test
-    @Disabled
     public void PhantomReadsProblemForOneToTwoRows() {
         final DbTable workDbTable = new DbTable(ONE_RECORD_AFTER_FIRST_INSERT_IMMUTABLE_DB_TABLE);
         logger.info("workDbTable = {}", workDbTable);
 
         final SQLCommandQueue sqlCommandQueue1 = new SQLCommandQueue();
         sqlCommandQueue1.add(
-                new SQLCommandSelect(workDbTable, Set.of(1, 2)),
+                new SQLCommandSelect(workDbTable, Set.of(0, 1, 2)),
                 new SQLCommandSleep(null, 200),
-                new SQLCommandSelect(workDbTable, Set.of(1, 2))
+                new SQLCommandSelect(workDbTable, Set.of(0, 1, 2))
         );
-        sqlCommandQueue1.startThread();
 
         final SQLCommandQueue sqlCommandQueue2 = new SQLCommandQueue();
         sqlCommandQueue2.add(
                 new SQLCommandSleep(null, 50),
-                new SQLCommandInsert(workDbTable, recordForOneInsert)
+                new SQLCommandInsert(workDbTable, recordAfterOneUpdate)
         );
-        sqlCommandQueue2.startThread();
 
+        sqlCommandQueue1.startThread();
+        sqlCommandQueue2.startThread();
         sqlCommandQueue1.joinToThread();
         sqlCommandQueue2.joinToThread();
 
-        ImmutableDbTable dbTableResultInTransaction2 = sqlCommandQueue1.popFromImmutableDbTableResultLog();
+        ImmutableDbTable dbTableResultInTransaction3 = sqlCommandQueue1.popFromImmutableDbTableResultLog();
+        sqlCommandQueue1.popFromImmutableDbTableResultLog();
         ImmutableDbTable dbTableResultInTransaction1 = sqlCommandQueue1.popFromImmutableDbTableResultLog();
 
         logger.info("dbTableResultInTransaction1 = {}", dbTableResultInTransaction1);
-        logger.info("dbTableResultInTransaction2 = {}", dbTableResultInTransaction2);
-        //  ToDo:   Сделать проверку на то, что количество строк НЕ совпадает.
-        Assertions.assertNotEquals(dbTableResultInTransaction1.count(), dbTableResultInTransaction2.count());
+        logger.info("dbTableResultInTransaction3 = {}", dbTableResultInTransaction3);
+        Assertions.assertNotEquals(dbTableResultInTransaction1.count(), dbTableResultInTransaction3.count());
     }
 }
