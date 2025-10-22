d99f763cc5d69d26b9177177c63929be068af9e2
timmaxx <m.n.timchenko@gmail.com>
2025-10-22 22:43:15 +0300

    prepare DbFields for ordering fields in records (other variant)

--- src/main/java/com/timmax/training_demo/transaction_isolation_level/v02/DbFields.java
+++ src/main/java/com/timmax/training_demo/transaction_isolation_level/v02/DbFields.java
@@ -1,67 +1,22 @@
 package com.timmax.training_demo.transaction_isolation_level.v02;
 
 import java.util.*;
-import java.util.stream.Collectors;
-
-import com.google.common.collect.ImmutableMap;
-
-/*
-public class Main {
-    public static void main(String[] args) {
-        Map<Integer, String> map = ImmutableMap.<Integer, String>builder()
-            .put(3, "Three")
-            .put(1, "One")
-            .put(2, "Two")
-            .build();
-
-        // Порядок сохраняется
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
-            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
-        }
-    }
-}
-*/
 
 public class DbFields {
-    private final ImmutableMap<DbFieldName, Class<?>> dbFields;
+    private final Map<DbFieldName, Class<?>> dbFields = new LinkedHashMap<>();
 
-    //  Warning:(10, 21) Raw use of parameterized class 'DbField'
-    public DbFields(DbField... dbFields) {
-        //  // Преобразование массива в Map с помощью Stream API
-        //        Map<String, Integer> map = Arrays.stream(pairs)
-        //                .collect(Collectors.toMap(
-        //                        Pair::getKey,    // Функция для получения ключа
-        //                        Pair::getValue   // Функция для получения значения
-        //                ));
-        //  Error:(42, 26) Incompatible types. Found: 'java.util.Map<com.timmax.training_demo.transaction_isolation_level.v02.DbFieldName,java.lang.Class>', required: 'java.util.Map<? extends com.timmax.training_demo.transaction_isolation_level.v02.DbFieldName,? extends java.lang.Class<?>>'
-        //  Error:(42, 26) Incompatible types. Found:
-        //  'java.util.Map<com.timmax.training_demo.transaction_isolation_level.v02.DbFieldName,java.lang.Class>'
-        //  , required:
-        //  'java.util.Map<? extends com.timmax.training_demo.transaction_isolation_level.v02.DbFieldName,? extends java.lang.Class<?>>'
-        this.dbFields = ImmutableMap.<DbFieldName, Class<?>>builder()
-                .putAll(Arrays
-                        .stream(dbFields)
-                        //  1.  Не уверен, что toMap создаст мапу упорядоченную так, как создаётся stream.
-                        //      А вдруг порядок перемешается?
-                        //  2.  Почему Idea подчеркивает красным collect?
-                        .collect(Collectors.toMap(DbField::getDbFieldName, DbField::getType)))
-                .build();
-/*
-        //  Это вариант с приведением типов
-        //  java: incompatible types: java.util.Map<com.timmax.training_demo.transaction_isolation_level.v02.DbFieldName,java.lang.Class> cannot be converted to java.util.Map<? extends com.timmax.training_demo.transaction_isolation_level.v02.DbFieldName,? extends java.lang.Class<?>>
-        //  java: incompatible types:
-        //  java.util.Map<com.timmax.training_demo.transaction_isolation_level.v02.DbFieldName,java.lang.Class>
-        //  cannot be converted to
-        //  java.util.Map<? extends com.timmax.training_demo.transaction_isolation_level.v02.DbFieldName,? extends java.lang.Class<?>>
-        this.dbFields = ImmutableMap.<DbFieldName, Class<?>>builder()
-                .putAll((Map<? extends DbFieldName, ? extends Class<?>>) Arrays
-                        .stream(dbFields)
-                        //  1.  Не уверен, что toMap создаст мапу упорядоченную так, как создаётся stream.
-                        //      А вдруг порядок перемешается?
-                        //  2.  Так Idea не подчеркивает красным collect, но подчеркивает желтым и НЕ компилирует.
-                        .collect(Collectors.toMap(DbField::getDbFieldName, DbField::getType)))
-                .build();
-*/
+    //  Warning:(9, 21) Raw use of parameterized class 'DbField'
+    public DbFields(DbField... arrayOfDbFields) {
+        Arrays.stream(arrayOfDbFields)
+                .forEach(dbField -> {
+                    if (dbFields.containsKey(dbField.getDbFieldName())) {
+                        throw new RuntimeException("Duplicate key: " + dbField.getDbFieldName());
+                    }
+                    if (dbField.getType() == null) {
+                        throw new RuntimeException("Type is null: " + dbField.getDbFieldName());
+                    }
+                    dbFields.put(dbField.getDbFieldName(), dbField.getType());
+                });
     }
 
     public Class<?> getDbFieldType(DbFieldName dbFieldName) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,4 @@
 module Transaction.isolation.level {
     requires org.java_websocket;
     requires java.sql;
-    requires com.google.common;
 }
