8bdc5ea0bd2e9bf52a149a082f827e2ab18b3c39
timmaxx <m.n.timchenko@gmail.com>
2026-01-18 18:09:53 +0300

    implement test for "Dirty write problem"

--- src/test/java/com/timmax/training_demo/transaction_isolation_level/v02/TransactionIsolationProblemTest.java
+++ src/test/java/com/timmax/training_demo/transaction_isolation_level/v02/TransactionIsolationProblemTest.java
@@ -12,6 +12,9 @@ import static com.timmax.training_demo.transaction_isolation_level.v02.DbTestDat
 //  Проблемы параллельного доступа с использованием транзакций
 //  https://ru.wikipedia.org/wiki/%D0%A3%D1%80%D0%BE%D0%B2%D0%B5%D0%BD%D1%8C_%D0%B8%D0%B7%D0%BE%D0%BB%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8_%D1%82%D1%80%D0%B0%D0%BD%D0%B7%D0%B0%D0%BA%D1%86%D0%B8%D0%B9
 
+//  A Critique of ANSI SQL Isolation Levels
+//  https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/tr-95-51.pdf
+
 public class TransactionIsolationProblemTest {
     DbTab dbTabPersonEmpty;
     DbTab dbTabPersonWithOneRow;
@@ -25,6 +28,65 @@ public class TransactionIsolationProblemTest {
         dbTabPersonWithOneRow = new DbTab(dbTabPersonRoWithOneRow, false);
         sqlCommandQueue1 = new SQLCommandQueue();
         sqlCommandQueue2 = new SQLCommandQueue();
+    }
+
+    //  0.  Dirty write - Грязная запись
+    @Test
+    public void dirtyWriteProblem() {
+        DbTab dbTabPerson = dbTabPersonWithOneRow;
+
+        //  --  Transaction 1:                      |   --  Transaction 2:
+        //  UPDATE person   --  1 row               |
+        //     SET name = name || " " || name;      |
+        //                                          |   UPDATE person   --  1 row
+        //                                          |      SET name = name || " " || name;
+        //  COMMIT; --  or ROLLBACK;                |
+        //                                          |  ROLLBACK;
+        //                                          |  SELECT *
+        //                                          |    FROM person;  --  1 row
+
+        sqlCommandQueue1.add(
+                //  Первый UPDATE изменит значение строки.
+                //  В журнале отката будет информация о возможном восстановлении до состояния перед первым UPDATE.
+                dbTabPerson.getDMLCommandUpdate(
+                        dbRec -> Map.of(
+                                DB_FIELD_NAME_NAME, dbRec.getValue(DB_FIELD_NAME_NAME) + " " + dbRec.getValue(DB_FIELD_NAME_NAME)
+                        )
+                )
+        );
+
+        sqlCommandQueue2.add(
+                //  Второй UPDATE изменит значение строки уже после того, как первый поменял.
+                //  В журнале отката будет информация о возможном восстановлении до состояния перед вторым UPDATE.
+                dbTabPerson.getDMLCommandUpdate(
+                        100L, 0L,
+                        dbRec -> Map.of(
+                                DB_FIELD_NAME_NAME, dbRec.getValue(DB_FIELD_NAME_NAME) + " " + dbRec.getValue(DB_FIELD_NAME_NAME)
+                        )
+                )
+        );
+
+        sqlCommandQueue1.startThread();
+        sqlCommandQueue2.startThread();
+        sqlCommandQueue1.joinToThread();
+        sqlCommandQueue2.joinToThread();
+
+        //  (Для COMMIT) Стираем журнал отката первой транзакции.
+        sqlCommandQueue1.commit();  //  or .rollback();
+        //  Откатываем вторую транзакцию, т.е. до состояния начала второго UPDATE,
+        //  т.е. как будто первый UPDATE выполнился.
+        sqlCommandQueue2.rollback();
+
+        sqlCommandQueue2.add(
+                dbTabPerson.getDQLCommandSelect()
+        );
+        sqlCommandQueue2.startThread();
+        sqlCommandQueue2.joinToThread();
+
+        DbSelect dbSelect2 = sqlCommandQueue2.popFromDQLResultLog();
+
+        //  Результирующая выборка должна получиться с учетом первого UPDATE.
+        DbSelectUtil.assertEquals(dbSelectPersonWithOneRow_BobBob, dbSelect2);
     }
 
     //  1.  Lost update - Потерянное обновление
